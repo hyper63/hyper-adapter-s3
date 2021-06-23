7e2b699ef20d4f047a00aa8ac1fddf08847f4cd3
Tyler Hall <0tillathehun0@gmail.com>
2021-06-23 15:34:23 -0400

    Initial commit

---
+++ dev_deps.js
@@ -0,0 +1,11 @@
+
+// Harness deps
+export { default as appOpine } from "https://raw.githubusercontent.com/hyper63/hyper63/main/packages/app-opine/mod.js";
+export { default as core } from "https://raw.githubusercontent.com/hyper63/hyper63/main/packages/core/mod.js";
+
+// Schema parsing deps
+export { default as validateFactorySchema } from "https://raw.githubusercontent.com/hyper63/hyper63/main/packages/core/utils/plugin-schema.js";
+export { data as validateDataAdapterSchema } from "https://raw.githubusercontent.com/hyper63/hyper63/main/packages/port-data/mod.js";
+
+// std lib deps
+export { assert } from "https://deno.land/std@0.98.0/testing/asserts.ts";
